727ef4f17ecb248269eb0f118a16c8d40c252c29
510shivam <136308575+510shivam@users.noreply.github.com>
2023-06-12 14:53:21 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,22 @@
+a = int(input("Enter the number:"))
+print ("your Grade is:" ,a)
+         
+if (a>80):
+    print("Grade is A+")
+
+elif (a>=60 and a<=80):
+    print("grade is A")
+
+elif (a>=50 and a<=60):
+    print("Grade is B+")
+
+elif (a>=45 and a<=50):
+    print("Grade is B")
+
+elif (a>=25 and a<45):
+    print("Grade is C")
+
+
+else:
+    Print("enter a valid number")    
+    
