c7a2b2baf010ae1eae99b3852ed3fc590de6b492
Shivam <shivamg@shivam.shivam>
2023-06-13 20:50:47 +0530

    commit message

---
+++ output.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo Hi user , welcome to election
+
+echo what is your age?
+
+read age
+if [ $age -ge 18 ]
+
+then
+	echo you can vote
+else
+	echo sorry you cannot vote!
+fi
+
+
